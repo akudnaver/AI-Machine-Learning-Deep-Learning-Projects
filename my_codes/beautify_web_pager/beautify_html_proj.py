42bcf80ac6f9819aa60e01cd59b5e1cfbe00f5d9
akudnaver <amit.kudnaver@gmail.com>
2020-03-23 16:42:42 +0530

    Update beautify_html_proj.py

--- my_codes/beautify_web_pager/beautify_html_proj.py
+++ my_codes/beautify_web_pager/beautify_html_proj.py
@@ -1,6 +1,6 @@
-# importing the libraries required to decode the HTML page and pull a table from a Web Page.
-# This code expects users to have fair amount of knoledge on how to read throuhg the HTML body,tags,classes. You can refer
-# the below links to develope an understanding of web page and for some basic foundation on Python Beautiful Soup functionality. 
+# importing the libraries required to access the HTML page and pull a table from a Web Page.
+# This code expects users to have fair amount of knowledge on how to read through the HTML body,tags,classes. You can refer
+# the below links to develope an understanding of HTML page and for some basic foundation on Python Beautiful Soup functionality. 
 
 import os
 import bs4
@@ -14,16 +14,16 @@ PATH = os.path.join('/','C:/', 'Users','akudnaver', 'Desktop')
 # Download the chromedriver.exe file and specif the path under PATH_CHROME
 PATH_CHROME = 'specify the path to chromedriver.exe' 
 
-# Here is a quick definition to iterate over each rows - 'tr' under a table body of your choice. We are basically
-# reading through the table rows 
+# Here is a quick definition/function to iterate over each rows ='tr' under a table body of your choice. We are basically
+# reading through the table rows from the webpage.
 
 def table_to_df(table):
     return pd.DataFrame([[td.text for td in row.find_all('td')] for row in soup.find_all('tr')])
 
 res = pd.DataFrame()
 
-# Here we are only interested in the website under variable URL , i will try to make generic in the my next project. 
-# You can use the webpage to play with the Beautiful soup function.
+# Here we are only interested in the website under variable 'URL' , i will try to make it generic in the my next project for
+# you to download all the tables of the webpage using Beautiful soup function.
 
 url = "https://www.meteoschweiz.admin.ch/home/messwerte.html"
 counter = 0
@@ -31,19 +31,19 @@ counter = 0
 # We will be using the selenium webdriver function to start the webpage we are interested to work with here. 
 chrome_options = webdriver.ChromeOptions()
 
-# a sandbox is security mechanism used to run an application in a restricted environment. 
-#If an attacker is able to exploit the browser in a way that lets him run arbitrary code on the machine, 
+# A sandbox is security mechanism used to run an application in a restricted environment. 
+# If an attacker is able to exploit the browser in a way that lets him run arbitrary code on the machine, 
 # the sandbox would help prevent this code from causing damage to the system. But in our case we would like to disbale 
 # the sandbox just to serve our purpose.
 chrome_options.add_argument('--no-sandbox')
 
 # Selenium python binding provides a simple API to write the functional tests using Selenium webdriver, with the help
 # of this tool, you can extract any element out of the webpage in a convenient way and as well you can perform tests on the
-# webpage of your choice.
+# webpage, of your choice.
 
 # In the below code we are creating an instance of Chrome webdriver and setting the URL our choice we wish to work with.
 
-driver = webdriver.Chrome(PATH', chrome_options=chrome_options)
+driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
 driver.get(url)
 
 
@@ -60,7 +60,7 @@ while True:
         print("html content:\n{}\n".format(page.content))
         continue
       
-    # In the below code we will add table to our empty data frame, the end result will have our table in the CSV format
+    # In the below code we will convert the table to a dataframe and save it in a CSV file
     res = res.append(table_to_df(table))
     res.dropna(axis=0,inplace=True)
     res.to_csv(os.path.join(PATH,"table.csv"), index=False, sep=',', header=None)
